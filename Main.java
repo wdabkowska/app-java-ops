04e4c6e11e49c4c4c306dd003b0dede440b707d9
wdabkowska <w.dabkowska.025@studms.ug.edu.pl>
2025-04-15 15:41:20 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Good morning, You!\n");
+        System.out.printf("Good morning, You!");
 
         String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
 
         for (int i = 1; i <= 4; i++) {
-            System.out.println("i = " + i + ", name = " + names[i - 1]);
+            System.out.println("" + i + " + names[i - 1]);
         }
     }
 }
